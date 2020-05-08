8f8f811be017cc9fd561ef4393e9b15e6f1fe59f
badr <badroxe.saadz@gmail.com>
2020-05-08 02:24:25 +0000

    Spring app with Spring Data MongoDB

---
+++ src/main/java/com/cap/mongodb/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.cap.mongodb;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends MongoRepository<Customer,String> {
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+}
